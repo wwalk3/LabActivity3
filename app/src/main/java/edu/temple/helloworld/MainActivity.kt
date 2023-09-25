80621cb2f75dc95d84df74a51f356cf2aca00f21
wwalk <wwalk831@gmail.com>
2023-09-25 16:28:35 -0400

    Created error message feature for user not entering name upon submission.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var clickMeButton: Button
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +20,24 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
 
-        
-        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+
+        clickMeButton.setOnClickListener {
+
+            val errorText = "Please enter a name"
+            val duration = Toast.LENGTH_SHORT
+            val errorToast = Toast.makeText(this, errorText, duration)
+
+
+            if (nameEditText.toString().isEmpty()) {
+                errorToast.show()
+            } else {
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
+
+
         }
-
-
     }
 }
